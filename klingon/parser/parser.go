4dd3ab9c0a3ac2fe15c996fce09dfd80e0b31635
pmukhin <pauri.lentonen@gmail.com>
2018-09-15 21:21:31 +0700

    Alphabet test + initial impl

--- klingon/parser/parser.go
+++ klingon/parser/parser.go
@@ -2,7 +2,7 @@ package parser
 
 import (
 	"errors"
-	"strings"
+	"fmt"
 )
 
 var (
@@ -14,9 +14,19 @@ type Parser interface {
 }
 
 type defaultParser struct {
-	src    []rune
+	src    []byte
 	offset int
 	len    int
+}
+
+func makeErr(r byte, pos int) error {
+	return errors.New(
+		fmt.Sprintf(
+			"unexpected character %s in pos %d",
+			string(r),
+			pos,
+		),
+	)
 }
 
 func (p defaultParser) Parse() ([]rune, error) {
@@ -37,21 +47,93 @@ func (p defaultParser) Parse() ([]rune, error) {
 func (p *defaultParser) getChar() (rune, error) {
 	for {
 		p.next()
-		if p.offset == p.len {
+		if p.offset >= p.len {
 			return -1, EOF
 		}
 		ch := p.src[p.offset]
 		switch ch {
-		case 'u':
-			return 0xF8E5, nil
-		case 'h':
-			return 0xF8D6, nil
-		case 'r':
-			return 0xF8E1, nil
-		case 'a':
+		case 'a', 'A':
 			return 0xF8D0, nil
+		case 'b', 'B':
+			return 0xF8D1, nil
+		case 'c', 'C':
+			nextCh := p.peek()
+			if nextCh == 'h' || nextCh == 'H' {
+				p.next()
+				return 0xF8D2, nil
+			} else {
+				return 0, makeErr(nextCh, p.offset+1)
+			}
+		case 'd', 'D':
+			return 0xF8D3, nil
+		case 'e', 'E':
+			return 0xF8D4, nil
+		case 'g', 'G':
+			nextCh := p.peek()
+			if nextCh == 'h' || nextCh == 'H' {
+				p.next()
+				return 0xF8D5, nil
+			} else {
+				return 0, makeErr(nextCh, p.offset+1)
+			}
+		case 'h', 'H':
+			return 0xF8D6, nil
+		case 'i', 'I':
+			return 0xF8D7, nil
+		case 'j', 'J':
+			return 0xF8D8, nil
+		case 'l', 'L':
+			return 0xF8D9, nil
+		case 'm', 'M':
+			return 0xF8DA, nil
+		case 'n', 'N':
+			nextCh := p.peek()
+			if nextCh == 'g' || nextCh == 'G' {
+				p.next()
+				return 0xF8DC, nil
+			} else {
+				return 0xF8DB, nil
+			}
+		case 'o', 'O':
+			return 0xF8DD, nil
+		case 'p', 'P':
+			return 0xF8DE, nil
+		case 'q':
+			return 0xF8DF, nil
+		case 'Q':
+			return 0xF8E0, nil
+		case 'r', 'R':
+			return 0xF8E1, nil
+		case 's', 'S':
+			return 0xF8E2, nil
+		case 't', 'T':
+			nextCh := p.peek()
+			if nextCh == 'l' || nextCh == 'L' {
+				p.next()
+				nextNextCh := p.peek()
+				if nextNextCh == 'h' || nextNextCh == 'H' {
+					p.next()
+					return 0xF8E4, nil
+				} else {
+					return 0, makeErr(nextCh, p.offset)
+				}
+			} else {
+				return 0xF8E3, nil
+			}
+		case 'u', 'U':
+			return 0xF8E5, nil
+		case 'v', 'V':
+			return 0xF8E6, nil
+		case 'w', 'W':
+			return 0xF8E7, nil
+		case 'y', 'Y':
+			return 0xF8E8, nil
+		case '\'':
+			return 0xF8E9, nil
+		case ' ':
+			return ' ', nil
 		default:
-			return -1, nil
+			return -1, makeErr(p.peek(), p.offset+1)
 		}
 	}
 }
@@ -60,9 +142,17 @@ func (p *defaultParser) next() {
 	p.offset++
 }
 
+func (p defaultParser) peek() byte {
+	if p.offset+1 >= p.len {
+		return 0
+	}
+	return p.src[p.offset+1]
+}
+
 func New(input string) Parser {
+	src := []byte(input)
 	return &defaultParser{
-		src:    []rune(strings.ToLower(input)),
+		src:    src,
 		offset: -1,
 		len:    len(input),
 	}
--- klingon/parser/parser_test.go
+++ klingon/parser/parser_test.go
@@ -11,9 +11,40 @@ func Test_defaultParser_Parse(t *testing.T) {
 		want    []rune
 		wantErr bool
 	}{
+		// breaking alphabet into groups for better readability
 		{
 			name:    "Uhura",
 			want:    []rune{0xF8E5, 0xF8D6, 0xF8E5, 0xF8E1, 0xF8D0},
+			wantErr: false,
+		},
+		{
+			name:    "abchDegh",
+			want:    []rune{0xF8D0, 0xF8D1, 0xF8D2, 0xF8D3, 0xF8D4, 0xF8D5},
+			wantErr: false,
+		},
+		{
+			name:    "HIjlmnng",
+			want:    []rune{0xF8D6, 0xF8D7, 0xF8D8, 0xF8D9, 0xF8DA, 0xF8DB, 0xF8DC},
+			wantErr: false,
+		},
+		{
+			name:    "opqQr",
+			want:    []rune{0xF8DD, 0xF8DE, 0xF8DF, 0xF8E0, 0xF8E1},
+			wantErr: false,
+		},
+		{
+			name:    "sttlhu",
+			want:    []rune{0xF8E2, 0xF8E3, 0xF8E4, 0xF8E5},
+			wantErr: false,
+		},
+		{
+			name:    "vwy'",
+			want:    []rune{0xF8E6, 0xF8E7, 0xF8E8, 0xF8E9},
+			wantErr: false,
+		},
+		{
+			name:    "vwy' v",
+			want:    []rune{0xF8E6, 0xF8E7, 0xF8E8, 0xF8E9, 0x20, 0xF8E6},
 			wantErr: false,
 		},
 	}
