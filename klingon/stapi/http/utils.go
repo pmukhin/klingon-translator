0dc088de0e81f2415ec4de49fdcefa3f6632f258
pmukhin <pauri.lentonen@gmail.com>
2018-09-17 20:47:06 +0700

    More descriptive name, using an interface instead of a type pointer

--- klingon/stapi/http/utils.go
+++ klingon/stapi/http/utils.go
@@ -3,8 +3,8 @@ package http
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
-	"net/http"
 )
 
 const (
@@ -16,8 +16,8 @@ const (
 // as it might be set to 0 by some vendor APIs
 //
 // Then content is fed to json.Unmarshal with given pointer
-func ReadAsJson(resp *http.Response, data interface{}) error {
-	jsonBytes, err := ioutil.ReadAll(resp.Body)
+func ReadAsJson(reader io.Reader, data interface{}) error {
+	jsonBytes, err := ioutil.ReadAll(reader)
 	if err != nil {
 		return fmt.Errorf(canNotRead, err.Error())
 	}
